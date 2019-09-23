e739c277c23b371f296baaa0ae3f515d903e57ee
unknown <syedmostaquimali@gmail.com>
2019-09-23 16:06:17 +0600

    setting changes- static, host

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bashachai.com']
 
 
 # Application definition
@@ -114,6 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 AUTH_USER_MODEL = 'accounts.User'
 
